36a70ba16b5d646548376d0339eb703ff9e9a37b
Mark Santiago <markangelob.santiago@gmail.com>
2021-09-04 12:37:13 -0700

    started navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem } from 'reactstrap';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -10,9 +10,47 @@ class App extends Component {
         return (
             <div className="App">
                 <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">Keyboard Builder</NavbarBrand>
-                </div>
+                  <div className="container">
+                      <NavbarBrand href="/">Keyboard Builder</NavbarBrand>
+                      <Nav>
+                        <NavItem>
+                          <NavLink href="/">
+                            Login
+                          </NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink href="/">
+                            Register
+                          </NavLink>
+                        </NavItem>  
+                      </Nav>
+                  </div>
+                </Navbar>
+                <Navbar>
+                  <div className="container">
+                    <Nav>
+                      <NavItem>
+                        <NavLink href="/">
+                          Plan Build
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink href="/">
+                          Building Tips
+                        </NavLink>
+                      </NavItem>  
+                      <NavItem>
+                        <NavLink href="/">
+                          Completed  Builds
+                        </NavLink>
+                      </NavItem>  
+                      <NavItem>
+                        <NavLink href="/">
+                          Upcoming
+                        </NavLink>
+                      </NavItem>  
+                    </Nav>
+                  </div>
                 </Navbar>
             </div>
         );

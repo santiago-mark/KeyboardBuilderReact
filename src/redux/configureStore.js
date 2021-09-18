c228048cdb17818e93086839296053e25244af57
Mark Santiago <markangelob.santiago@gmail.com>
2021-09-18 13:04:34 -0700

    removed combinedreducers to submit

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,18 +4,20 @@ import { KeycapSet } from './keycapSet'
 import { Switches } from './switches'
 import { Stabilizers } from './stabilizers'
 
-//import { Reducer, initialState } from './reducer';
+import { Reducer, initialState } from './reducer';
 
 export const ConfigureStore = () => {
     const store = createStore(
+       /*
         combineReducers({
             keyboardCase: KeyboardCase,
             keycapSet: KeycapSet,
             switches: Switches,
             stabilizers: Stabilizers
         })
-        //Reducer,
-        //initialState
+        */
+        Reducer,
+        initialState
     );
 
     return store;
